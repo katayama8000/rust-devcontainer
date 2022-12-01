57e18a8d8f3cc7653e660b6c8211b143c19ae795
katayama tatsuhumi <t-katayama@cries.co.jp>
2022-12-01 16:04:51 +0900

    update

--- vue3-vite-ts/src/App.vue
+++ vue3-vite-ts/src/App.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import leftList from "./components/LeftList.vue";
 import rightList from "./components/RightList.vue";
+
+const inputVal = ref("");
+const handleClick = () => {
+  if(inputVal.value === "")  {
+    alert("何か入力してください");
+    return
+  }
+  console.log(inputVal.value);
+};
 </script>
 
 <template>
   <div class="flex w-[700px] m-auto justify-between mt-10">
-    <leftList msg="leftList" />
-    <rightList msg="rightList" />
+    <!-- <leftList msg="leftList" />
+    <rightList msg="rightList" /> -->
+    <form type="text" >
+      <input type="text" v-model="inputVal" />
+      <button type="button" @click="handleClick()">test
+      </button>
+    </form>
   </div>
 </template>
 
