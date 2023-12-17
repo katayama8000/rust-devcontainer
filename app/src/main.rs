708e57e43e42063e394949bd45963570fb1c5f7f
katayama800 <tattu.0310@gmail.com>
2023-12-17 09:47:47 +0000

    wip

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,17 @@
+use expo_server_sdk::{get_receipts, push_message};
 use lib_demo::{add, print_random_number};
-fn main() {
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
     println!("1 + 2 = {}", add(1, 2));
     print_random_number();
+    let title = "sdk-test-title";
+    let body = "sdk-test-body";
+    let push_token = "ExponentPushToken[b5nR6zALafV431QtOC7b";
+    let ret = match push_message(&[push_token], title, body).await {
+        Ok(val) => val,
+        Err(_e) => todo!(),
+    };
+    println!("{}", ret.data[0].id)
 }
