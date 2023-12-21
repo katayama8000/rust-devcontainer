5286ae3a5911090d33721d2dcca9f67933cfdbcc
katayama800 <tattu.0310@gmail.com>
2023-12-21 09:56:33 +0000

    add

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,35 +1,79 @@
-use lib_demo::{add, print_random_number};
-use push::{push_message, Error, PushTicket};
-mod push;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum CustomError {
+    InvalidArgument(String),
+    DeserializeErr(String),
+    ServerErr(String),
+}
+
+pub mod get;
+pub mod push;
+use get::get_push_receipts;
+use push::{send_push_notification, PushTicket};
+
+use crate::get::PushReceipt;
+
+// Assuming `CustomError` and other necessary modules are imported appropriately.
 
 #[tokio::main]
 async fn main() {
-    add(1, 1);
-    print_random_number();
-    let push_token = [
+    // Replace these values with actual Expo Push Tokens and other relevant data
+
+    let expo_push_tokens = [
         "ExponentPushToken[GG5W7qB0nelNDkz5Y6A0sB]",
         "ExponentPushToken[GG5W7qB0nelNDkz5Y6A0s]",
     ];
     let title = "Hello";
     let body = "World";
 
-    let ret: Result<Vec<String>, Error> = match push_message(&push_token, title, body).await {
-        Ok(super_responses) => {
-            let mut ids = Vec::new();
-            for super_response in super_responses {
-                match super_response {
-                    PushTicket::Success(push_ticket) => {
-                        println!("PushTicket: {:?}", push_ticket);
-                        ids.push(push_ticket.id);
+    // Example of sending a push notification
+    let ids: Vec<String> = match send_push_notification(
+        expo_push_tokens.iter().map(|s| s.to_string()).collect(),
+        title.to_string(),
+        body.to_string(),
+    )
+    .await
+    {
+        Ok(push_tickets) => {
+            println!("Push notification sent successfully!");
+            let mut receipt_ids = Vec::new();
+            for push_ticket in push_tickets {
+                match push_ticket {
+                    PushTicket::Success(success_ticket) => {
+                        println!("Success Ticket: {:?}", success_ticket);
+                        receipt_ids.push(success_ticket.id);
                     }
-                    PushTicket::Error(error_response) => {
-                        println!("Error: {:?}", error_response);
+                    PushTicket::Error(error_ticket) => {
+                        println!("Error Ticket: {:?}", error_ticket);
                     }
                 }
             }
-            Ok(ids)
+            receipt_ids
         }
-        Err(error) => Err(error),
+        Err(error) => {
+            eprintln!("Failed to send push notification: {:?}", error);
+            vec![]
+        }
     };
-    println!("ret: {:?}", ret.unwrap());
+
+    // Example of getting push receipts
+    match get_push_receipts(ids.clone()).await {
+        Ok(push_receipts) => {
+            println!("Push receipts received successfully!");
+            for push_receipt in push_receipts {
+                match push_receipt {
+                    PushReceipt::Success(success_receipt) => {
+                        println!("Success Receipt: {:?}", success_receipt);
+                    }
+                    PushReceipt::Error(error_receipts) => {
+                        println!("Error Receipts: {:?}", error_receipts);
+                    }
+                }
+            }
+        }
+        Err(error) => {
+            eprintln!("Failed to get push receipts: {:?}", error);
+        }
+    }
 }
--- app/src/push.rs
+++ app/src/push.rs
@@ -1,5 +1,13 @@
 use reqwest::header::{HeaderMap, HeaderValue, CONTENT_TYPE};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum CustomError {
+    InvalidArgument(String),
+    DeserializeErr(String),
+    ServerErr(String),
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum PushTicket {
@@ -17,55 +25,43 @@ pub struct PushSuccessTicket {
 pub struct PushErrorTicket {
     pub status: String,
     pub message: String,
-    pub details: Details,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Default)]
-pub struct Details {
-    pub error: String,
-}
-
-#[derive(Debug, Deserialize, PartialEq)]
-pub enum Error {
-    InvalidArgument(String),
-    DeserializeErr(String),
-    ServerErr(String),
+    pub details: Value,
 }
 
 #[derive(Debug, Serialize)]
-pub struct PushPayload<'a> {
-    to: &'a [&'a str],
-    title: &'a str,
-    body: &'a str,
+pub struct PushPayload {
+    to: Vec<String>,
+    title: String,
+    body: String,
 }
 
 #[derive(Debug, Deserialize)]
 struct PushResult {
-    data: Vec<ResultEntry>,
+    data: Vec<PushResultItem>,
 }
 
 #[derive(Debug, Deserialize)]
-struct ResultEntry {
+struct PushResultItem {
     status: String,
     id: Option<String>,
     message: Option<String>,
-    details: Option<Details>,
+    details: Option<Value>,
 }
 
-pub async fn push_message(
-    expo_push_tokens: &[&str],
-    title: &str,
-    body: &str,
-) -> Result<Vec<PushTicket>, Error> {
+pub async fn send_push_notification(
+    expo_push_tokens: Vec<String>,
+    title: String,
+    body: String,
+) -> Result<Vec<PushTicket>, CustomError> {
     const URL: &str = "https://exp.host/--/api/v2/push/send";
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
 
     let client = reqwest::Client::new();
 
-    for token in expo_push_tokens {
+    for token in expo_push_tokens.clone() {
         if !token.starts_with("ExponentPushToken[") {
-            return Err(Error::InvalidArgument(format!(
+            return Err(CustomError::InvalidArgument(format!(
                 "Invalid expo push token: {}",
                 token
             )));
@@ -73,18 +69,18 @@ pub async fn push_message(
     }
 
     if title.is_empty() {
-        return Err(Error::InvalidArgument("Title is empty".to_string()));
+        return Err(CustomError::InvalidArgument("Title is empty".to_string()));
     }
 
     if body.is_empty() {
-        return Err(Error::InvalidArgument("Body is empty".to_string()));
+        return Err(CustomError::InvalidArgument("Body is empty".to_string()));
     }
 
-    let payload = PushPayload {
-        to: expo_push_tokens,
-        title,
-        body,
-    };
+    let payload = json!({
+        "to": expo_push_tokens,
+        "title": title,
+        "body": body,
+    });
 
     match client
         .post(URL)
@@ -99,7 +95,10 @@ pub async fn push_message(
                     .json::<PushResult>()
                     .await
                     .map_err(|err| {
-                        Error::DeserializeErr(format!("Failed to deserialize response: {:?}", err))
+                        CustomError::DeserializeErr(format!(
+                            "Failed to deserialize response: {:?}",
+                            err
+                        ))
                     })?
                     .data
                     .into_iter()
@@ -107,13 +106,13 @@ pub async fn push_message(
                         if item.status == "error" {
                             PushTicket::Error(PushErrorTicket {
                                 status: item.status,
-                                message: item.message.unwrap_or_default(), // Use unwrap_or_default to provide a default value
-                                details: item.details.unwrap_or_default(), // Use unwrap_or_default to provide a default value
+                                message: item.message.expect("message is empty"),
+                                details: item.details.expect("details is empty"),
                             })
                         } else if item.status == "ok" {
                             PushTicket::Success(PushSuccessTicket {
-                                status: "ok".to_string(),
-                                id: item.id.unwrap_or_default(), // Use unwrap_or_default to provide a default value
+                                status: item.status,
+                                id: item.id.expect("id is empty"),
                             })
                         } else {
                             unreachable!("Unknown status: {}", item.status)
@@ -121,13 +120,13 @@ pub async fn push_message(
                     })
                     .collect())
             } else {
-                Err(Error::ServerErr(format!(
+                Err(CustomError::ServerErr(format!(
                     "Failed to send request: {:?}",
                     response
                 )))
             }
         }
-        Err(err) => Err(Error::ServerErr(format!(
+        Err(err) => Err(CustomError::ServerErr(format!(
             "Failed to send request: {:?}",
             err
         ))),
