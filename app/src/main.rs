9a2bff75cd87a340598bb3fff06620360aa056bb
katayama800 <tattu.0310@gmail.com>
2023-12-23 05:19:38 +0000

    update

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,9 @@
+use std::collections::HashMap;
+
 use expo_server_sdk::{expo::expo::Expo, object::expo_push_message::ExpoPushMessage};
+use serde::{Deserialize, Deserializer};
+use serde_derive::Serialize;
+use serde_with::skip_serializing_none;
 
 #[tokio::main]
 async fn main() {
@@ -9,9 +14,39 @@ async fn main() {
     let title = "Hello".to_string();
     let body = "World".to_string();
 
-    let expo_message = ExpoPushMessage::new(expo_push_tokens, title, body);
+    // let expo_message = ExpoPushMessage::new(expo_push_tokens, title, body);
 
-    let expo = Expo::new(expo_server_sdk::expo::expo::ExpoClientOptions { access_token: None });
-    let result = expo.send_push_notifications(expo_message).await;
-    println!("{:?}", result)
+    // let expo = Expo::new(expo_server_sdk::expo::expo::ExpoClientOptions { access_token: None });
+    // let result = expo.send_push_notifications(expo_message).await;
+    // println!("{:?}", result)
+
+    let message = ExpoPushMessage {
+        to: vec!["user1".to_string(), "user2".to_string()],
+        title: "My title".to_string(),
+        body: "My body".to_string(),
+        data: Some(HashMap::new()),
+        ttl: None,
+        expiration: None,
+        priority: None,
+        subtitle: None,
+        sound: None,
+        badge: None,
+        channel_id: None,
+        category_id: None,
+        mutable_content: None,
+    };
+
+    #[skip_serializing_none]
+    #[derive(Debug, Deserialize, Serialize)]
+    struct Hoge {
+        val1: i32,
+        val2: Option<u64>,
+    }
+    let hoge = Hoge {
+        val1: 1,
+        val2: None,
+    };
+    // jsonに変換
+    let json = serde_json::to_string(&hoge).unwrap();
+    println!("{}", json);
 }
