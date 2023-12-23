81738643903385215e8e03d9cbb637adc2148a30
katayama800 <tattu.0310@gmail.com>
2023-12-23 09:02:02 +0000

    expo_push_notification_client

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,21 +1,17 @@
-use expo_server_sdk::{Expo, ExpoClientOptions, ExpoPushMessage, ExpoPushReceiptId};
+use expo_push_notification_client::{Expo, ExpoClientOptions, ExpoPushMessage};
 
 #[tokio::main]
 async fn main() {
-    let access_token = "xxxxx".to_string();
     let expo_push_tokens = vec![
-        "ExponentPushToken[xxxxx]".to_string(),
-        "ExponentPushToken[xxxxx]".to_string(),
+        "ExponentPushToken[GG5W7qB0nelNDkz5Y6A0sB]".to_string(),
+        "ExponentPushToken[GG5W7qB0nelNDkz5Y6A0sB]".to_string(),
     ];
     let title = "Hello".to_string();
     let body = "World".to_string();
 
-    let expo = Expo::new(ExpoClientOptions {
-        access_token: Some(access_token),
-    });
-    let expo_push_message = ExpoPushMessage::new(expo_push_tokens, title, body);
-    expo.send_push_notifications(expo_push_message).await;
+    let expo_message = ExpoPushMessage::new(expo_push_tokens, title, body);
 
-    let expo_push_ids = ExpoPushReceiptId::new(vec!["xxxxx".to_string(), "xxxxx".to_string()]);
-    expo.get_push_notification_receipts(expo_push_ids).await;
+    let expo = Expo::new(ExpoClientOptions { access_token: None });
+    let result = expo.send_push_notifications(expo_message).await;
+    println!("{:?}", result);
 }
