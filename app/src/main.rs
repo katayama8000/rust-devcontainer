a3b9bdbf7a6612f0d372661c0e361b49093bf08c
katayama800 <tattu.0310@gmail.com>
2023-12-23 08:42:05 +0000

    add

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,23 +1,21 @@
-use expo_server_sdk::{Expo, ExpoPushMessage, ExpoPushTicket};
+use expo_server_sdk::{Expo, ExpoClientOptions, ExpoPushMessage, ExpoPushReceiptId};
 
 #[tokio::main]
 async fn main() {
+    let access_token = "xxxxx".to_string();
     let expo_push_tokens = vec![
-        "ExponentPushToken[GG5W7qB0nelNDkz5Y6A0sB]".to_string(),
-        "ExponentPushToken[GG5W7qB0nelNDkz5Y6A0sB]".to_string(),
+        "ExponentPushToken[xxxxx]".to_string(),
+        "ExponentPushToken[xxxxx]".to_string(),
     ];
     let title = "Hello".to_string();
     let body = "World".to_string();
 
-    // let mut data = HashMap::new();
-    // data.insert("key1".to_string());
+    let expo = Expo::new(ExpoClientOptions {
+        access_token: Some(access_token),
+    });
+    let expo_push_message = ExpoPushMessage::new(expo_push_tokens, title, body);
+    expo.send_push_notifications(expo_push_message).await;
 
-    let expo_message = ExpoPushMessage::new(expo_push_tokens, title, body)
-        .badge(10)
-        .mutable_content(true);
-
-    let expo = Expo::new(expo_server_sdk::expo::expo::ExpoClientOptions { access_token: None });
-    let result: Result<Vec<ExpoPushTicket>, expo_server_sdk::error::CustomError> =
-        expo.send_push_notifications(expo_message).await;
-    println!("{:?}", result)
+    let expo_push_ids = ExpoPushReceiptId::new(vec!["xxxxx".to_string(), "xxxxx".to_string()]);
+    expo.get_push_notification_receipts(expo_push_ids).await;
 }
