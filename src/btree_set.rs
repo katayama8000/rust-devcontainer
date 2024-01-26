3fdc4abd6c46156b55d758b15f214de26351c395
katayama800 <tattu.0310@gmail.com>
2024-01-26 14:18:56 +0000

    btree_set

--- src/btree_set.rs
+++ src/btree_set.rs
@@ -8,8 +8,26 @@ pub fn run() {
     let mut set = BTreeSet::new();
     set.insert(1);
     set.insert(2);
+    println!("{:?}", set);
     // append
-    set.append(&mut BTreeSet::new());
+    let mut set2: BTreeSet<i32> = BTreeSet::new();
+    set2.insert(3);
+    set2.insert(4);
+    println!("{:?}", set2);
+    set.append(&mut set2);
+    println!("{:?}", set);
+    // contains
+    println!("{}", set.contains(&1));
+    // len
+    println!("{}", set.len());
+    // is_empty
+    println!("{}", set.is_empty());
+    // remove
+    set.remove(&1);
+    println!("{:?}", set);
+    // clear
+    set.clear();
+    println!("{:?}", set);
 }
 
 // test
