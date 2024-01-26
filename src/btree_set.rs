f0e89a0e74e756ec83232b24056093205b9f7074
katayama800 <tattu.0310@gmail.com>
2024-01-26 03:59:30 +0000

    btree

--- src/btree_set.rs
+++ src/btree_set.rs
@@ -1,6 +1,28 @@
+use std::collections::BTreeSet;
 pub fn run() {
     println!(
         "{}{}{}btree_set.rs{}{}{}",
         "🦀", "🦀", "🦀", "🦀", "🦀", "🦀"
     );
+
+    let mut set = BTreeSet::new();
+    set.insert(1);
+    set.insert(2);
+    // append
+    set.append(&mut BTreeSet::new());
+}
+
+// test
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let mut set = BTreeSet::new();
+        set.insert(1);
+        set.insert(2);
+        assert_eq!(set.contains(&1), true);
+        assert_eq!(set.len(), 2);
+        set.append(&mut BTreeSet::new());
+    }
 }
