6dc31cc108cfa8b11ee3094a8735adb6ca4fc756
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2024-12-14 23:09:12 +0900

    wasm (#5)
    * Add restaurant module and update dependencies
    
    * hello
    
    * Update Dockerfile and add WebAssembly support with hello-wasm module
    
    * Add .gitattributes to configure linguist language detection for markdown, yaml, and toml files

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,17 @@
+mod front_of_house;
+pub use self::front_of_house::hosting;
+// use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant2() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
